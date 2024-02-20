5e992b8b65420bccf0d65d97d44e82c22f63f8e2
cuitin <s0083431@bis-school.com>
2024-02-20 23:06:02 +0000

    Workinging solution to S1 2022 (Not full marks)

--- CCC/CCC2022/S1_Good_Fours_and_Good_Fives.java
+++ CCC/CCC2022/S1_Good_Fours_and_Good_Fives.java
@@ -4,31 +4,21 @@ import java.util.*;
 
 public class S1_Good_Fours_and_Good_Fives
 {
-    static HashMap<Integer, Integer> memo; // Memoized solutions
-    public static int get_combinations(int N)
-    {
-        if (! Objects.isNull(memo.get(N))) {
-            return memo.get(N);
-        }
-        else if (N < 4) {
-            memo.put(N, 0);
-            return 0;
-        }
-        else {
-            int L = N-5;
-            int R = N-4;
-            int combi = get_combinations(L) + get_combinations(R);
-            memo.put(N, combi);
-            return combi;
-        }
-    }
     public static void main(String[] args) {
+        // 4a + 5b = N
         Scanner scan = new Scanner(System.in);
-        memo.put(4, 1);
-        memo.put(5, 1);
 
         int N = Integer.parseInt(scan.nextLine());
 
-        System.out.println(get_combinations(N));
+        int possible = 0;
+        for (int a=0; a<=N/4; a++) {
+            for (int b=0; b<=N/5; b++) {
+                if (4*a + 5*b == N) {
+                    possible++;
+                }
+            }
+        }
+
+        System.out.println(possible);
     }
 }
